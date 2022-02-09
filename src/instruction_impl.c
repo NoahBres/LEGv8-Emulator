a82df4ddc019dcd71112b7127f2a3d594bfc9e5a
Noah Bresler <noahbresler2002@gmail.com>
2022-02-08 23:18:40 -0500

    Fix long long printf types for llvm

--- src/instruction_impl.c
+++ src/instruction_impl.c
@@ -226,23 +226,22 @@ char printable_char(uint8_t c) { return isprint(c) ? c : '.'; }
 void print_hexdump(int8_t *start, size_t size) {
   size_t i;
   for (i = 0; i < size - (size % 16); i += 16) {
-    printf(
-        "%08x "
-        " %02hhx %02hhx %02hhx %02hhx %02hhx %02hhx %02hhx %02hhx "
-        " %02hhx %02hhx %02hhx %02hhx %02hhx %02hhx %02hhx %02hhx "
-        " |%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c|\n",
-        (int32_t)i, start[i + 0], start[i + 1], start[i + 2], start[i + 3],
-        start[i + 4], start[i + 5], start[i + 6], start[i + 7], start[i + 8],
-        start[i + 9], start[i + 10], start[i + 11], start[i + 12],
-        start[i + 13], start[i + 14], start[i + 15],
-        printable_char(start[i + 0]), printable_char(start[i + 1]),
-        printable_char(start[i + 2]), printable_char(start[i + 3]),
-        printable_char(start[i + 4]), printable_char(start[i + 5]),
-        printable_char(start[i + 6]), printable_char(start[i + 7]),
-        printable_char(start[i + 8]), printable_char(start[i + 9]),
-        printable_char(start[i + 10]), printable_char(start[i + 11]),
-        printable_char(start[i + 12]), printable_char(start[i + 13]),
-        printable_char(start[i + 14]), printable_char(start[i + 15]));
+    printf("%08x "
+           " %02hhx %02hhx %02hhx %02hhx %02hhx %02hhx %02hhx %02hhx "
+           " %02hhx %02hhx %02hhx %02hhx %02hhx %02hhx %02hhx %02hhx "
+           " |%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c|\n",
+           (int32_t)i, start[i + 0], start[i + 1], start[i + 2], start[i + 3],
+           start[i + 4], start[i + 5], start[i + 6], start[i + 7], start[i + 8],
+           start[i + 9], start[i + 10], start[i + 11], start[i + 12],
+           start[i + 13], start[i + 14], start[i + 15],
+           printable_char(start[i + 0]), printable_char(start[i + 1]),
+           printable_char(start[i + 2]), printable_char(start[i + 3]),
+           printable_char(start[i + 4]), printable_char(start[i + 5]),
+           printable_char(start[i + 6]), printable_char(start[i + 7]),
+           printable_char(start[i + 8]), printable_char(start[i + 9]),
+           printable_char(start[i + 10]), printable_char(start[i + 11]),
+           printable_char(start[i + 12]), printable_char(start[i + 13]),
+           printable_char(start[i + 14]), printable_char(start[i + 15]));
   }
   printf("%08x\n", (int32_t)size);
 }
@@ -277,28 +276,28 @@ void DUMP(machine_state_t *machine_state, instruction_t *instr) {
   char *reg_name = "   ";
   for (int i = 0; i < REG_SIZE; i++) {
     switch (i) {
-      case PC:
-        reg_name = "(PC)";
-        break;
-      case IP1:
-        reg_name = "(IP1)";
-        break;
-      case SP:
-        reg_name = "(SP)";
-        break;
-      case FP:
-        reg_name = "(FP)";
-        break;
-      case LR:
-        reg_name = "(LR)";
-        break;
-      case XZR:
-        reg_name = "(XZR)";
-        break;
-      default:
-        reg_name = "   ";
+    case PC:
+      reg_name = "(PC)";
+      break;
+    case IP1:
+      reg_name = "(IP1)";
+      break;
+    case SP:
+      reg_name = "(SP)";
+      break;
+    case FP:
+      reg_name = "(FP)";
+      break;
+    case LR:
+      reg_name = "(LR)";
+      break;
+    case XZR:
+      reg_name = "(XZR)";
+      break;
+    default:
+      reg_name = "   ";
     }
-    printf("%6s X%02d: 0x%016lx (%d)\n", reg_name, i,
+    printf("%6s X%02d: 0x%016llx (%d)\n", reg_name, i,
            (uint64_t)machine_state->X[i], (int)machine_state->X[i]);
   }
 
@@ -321,7 +320,7 @@ void PRNL(machine_state_t *machine_state, instruction_t *instr) {
 }
 
 void PRNT(machine_state_t *machine_state, instruction_t *instr) {
-  printf("X%02d: 0x%016lx (%d)\n", instr->R.Rd,
+  printf("X%02d: 0x%016llx (%d)\n", instr->R.Rd,
          (uint64_t)machine_state->X[instr->R.Rd],
          (int)machine_state->X[instr->R.Rd]);
 }
